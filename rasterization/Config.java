13cfb6e391c67e3a205e6f9f916d249178a60f8f
Kermo Saarse <saarse@hotmail.com>
2021-12-09 20:57:20 +0200

    Optimized Config.java

--- rasterization/Config.java
+++ rasterization/Config.java
@@ -4,18 +4,20 @@ import java.awt.Toolkit;
 
 public class Config {
     private static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
-    public static final int SCREENSIZE_X = TOOLKIT.getScreenSize().width;
-    public static final int SCREENSIZE_Y = TOOLKIT.getScreenSize().height;
-
-    public static final double CAMERA_FOV = 70;
-    public static final boolean ANTI_ALIASING = true;
-
-    public static final boolean SPECULAR_HIGHLIGHTS = true;
-    public static final boolean SHADOWS = true;
-    public static final int SHADOW_RESOLUTION = ANTI_ALIASING ? 4 * SCREENSIZE_X : 2 * SCREENSIZE_X;
     
-    public static final double 
+    public static final int 
+        SCREENSIZE_X = TOOLKIT.getScreenSize().width,
+        SCREENSIZE_Y = TOOLKIT.getScreenSize().height;
+
+    public static final int SHADOW_RESOLUTION = 2 * SCREENSIZE_X;
+
+    public static final boolean 
+        SPECULAR_HIGHLIGHTS = true, 
+        SHADOWS = true;
+    
+    public static final double
+        CAMERA_FOV = 70, 
+        SHADOWMAP_FOV = 120,
         DEPTHMAP_BIAS = 0.001,
-        SHADOWMAP_BIAS = 0.001,
-        SHADOWMAP_FOV = 120;
+        SHADOWMAP_BIAS = 0.001;
 }
